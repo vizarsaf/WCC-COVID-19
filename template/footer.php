9e50ba4c760e37d04939b4f35f0d9773c7a2e2ae
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-25 14:06:55 +0700

    Add template

---
+++ template/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white d-print-none">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <!-- <span>Copyright &copy; Your Website 2019</span> -->
+      <span><a href="https://widuricomputercommunity.com" target="_blank">Widuri Computer Community</a> - <a href="LICENSE.txt" target="_blank">Copyright (c) 2020 WCC</a></span>
+    </div>
+  </div>
+</footer>
+<!-- End of Footer -->
---
+++ template/sidebar.php
@@ -0,0 +1,28 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion d-print-none" id="accordionSidebar">
+
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+    <div class="sidebar-brand-icon rotate-n-15">
+      <i class="fas fa-shield-virus"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">COVID-19 By WCC</div>
+  </a>
+
+  <hr class="sidebar-divider my-0">
+
+  <li class="nav-item active">
+    <a class="nav-link" href="?page=dashboard">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span></a>
+  </li>
+
+  <hr class="sidebar-divider">
+
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
+
+</ul>
+<!-- End of Sidebar -->
