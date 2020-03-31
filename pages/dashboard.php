a18afd2e17b359280dcd8193251f3ac26f8ca490
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-31 12:54:40 +0700

    Semua data berdasarkan database.

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,6 +1,15 @@
 <div class="d-sm-flex align-items-center justify-content-between my-4">
   <h3 class="h3">DKI Jakarta</h3>
-  <h5 class="h5">(Last Update: 2020 Maret 28 10:00)<a href="https://corona.jakarta.go.id/">corona.jakarta.go.id</a></h5>
+  <?php
+    $data_jakarta = mysqli_query($con, "SELECT positif, sembuh, meninggal, last_update FROM provinsi WHERE nama='Jakarta'");
+    while ($row = mysqli_fetch_array($data_jakarta)) {
+      $data_jakarta_positif     = $row['positif'];
+      $data_jakarta_sembuh      = $row['sembuh'];
+      $data_jakarta_meninggal   = $row['meninggal'];
+      $data_jakarta_last_update = $row['last_update'];
+    }
+  ?>
+  <h5 class="h5">(Last Update: <?= $data_jakarta_last_update; ?>)<a href="https://corona.jakarta.go.id/">corona.jakarta.go.id</a></h5>
 </div>
 
 <div class="row">
@@ -15,12 +24,7 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-uppercase mb-1">Positif COVID-19</div>
-                <div class="h5 mb-0 font-weight-bold">
-                  <?php
-                    $data_jakarta = mysqli_query($con, "SELECT positif, sembuh, meninggal FROM provinsi WHERE nama='Jakarta'");
-                    while ($row = mysqli_fetch_array($data_jakarta)) echo $row['positif'];
-                  ?>
-                </div>
+                <div class="h5 mb-0 font-weight-bold"><?= $data_jakarta_positif; ?></div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-head-side-cough fa-2x"></i>
@@ -36,12 +40,7 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-uppercase mb-1">Sembuh</div>
-                <div class="h5 mb-0 font-weight-bold">
-                  <?php
-                    $data_jakarta = mysqli_query($con, "SELECT positif, sembuh, meninggal FROM provinsi WHERE nama='Jakarta'");
-                    while ($row = mysqli_fetch_array($data_jakarta)) echo $row['sembuh'];
-                  ?>
-                </div>
+                <div class="h5 mb-0 font-weight-bold"><?= $data_jakarta_sembuh; ?></div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-notes-medical fa-2x"></i>
@@ -57,12 +56,7 @@
             <div class="row no-gutters align-items-center">
               <div class="col mr-2">
                 <div class="text-xs font-weight-bold text-uppercase mb-1">Meninggal</div>
-                <div class="h5 mb-0 font-weight-bold">
-                  <?php
-                    $data_jakarta = mysqli_query($con, "SELECT positif, sembuh, meninggal FROM provinsi WHERE nama='Jakarta'");
-                    while ($row = mysqli_fetch_array($data_jakarta)) echo $row['meninggal'];
-                  ?>
-                </div>
+                <div class="h5 mb-0 font-weight-bold"><?= $data_jakarta_Meninggal; ?></div>
               </div>
               <div class="col-auto">
                 <i class="fas fa-skull-crossbones fa-2x"></i>
@@ -72,7 +66,7 @@
         </div>
       </div>
 
-      <div class="col-md-3 mb-4">
+      <!-- <div class="col-md-3 mb-4">
         <div class="card text-gray-100 shadow h-100 py-2" style="background:#2060A7;">
           <div class="card-body">
             <div class="row no-gutters align-items-center">
@@ -82,11 +76,12 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> -->
       <!-- End col timeline -->
 
       <!-- Peta disini -->
-      <div class="col-md-9 mb-4">
+      <!-- <div class="col-md-9 mb-4"> -->
+      <div class="col-md-12 mb-4">
         <div class="card text-gray-100 shadow h-100 p-0" style="background:#2060A7;">
 
           <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqEEJqD-6CS1FMneeEsgVRViLE8i0Tmgw&callback=initMap&libraries=drawing&pb=!1m18!1m12!1m3!1d7933.03325458806!2d106.7975101741862!3d-6.19535793205976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f6965c21cf89%3A0xb4e9aae2a244be30!2sSlipi%2C+Palmerah%2C+West+Jakarta+City%2C+Jakarta!5e0!3m2!1sen!2sid!4v1560134937615!5m2!1sen!2sid"></script>
