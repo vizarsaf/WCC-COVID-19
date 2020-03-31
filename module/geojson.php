4a2ca7a6df674e7b0ddb03ca3b0078dda5190dd7
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-31 17:47:39 +0700

    test geojson provinsi dihapus

--- module/geojson.php
+++ module/geojson.php
@@ -9,36 +9,6 @@
 
     <?php $i = 0; ?>
     
-    <?php
-      $sql = "SELECT *
-        FROM provinsi
-        WHERE polygon != ''
-        AND nama != 'Jakarta'
-      ";
-      $result = $con->query($sql);
-      while ($row = $result->fetch_assoc()) { $i++; if ($i>1) echo ",";
-    ?>
-    {
-      "type":"Feature",
-      "properties":{
-        "table":"provinsi",
-        "provinsi":"<?= $row["nama"]; ?>",
-        "positif":"<?= $row["positif"]; ?>",
-        "sembuh":"<?= $row["sembuh"]; ?>",
-        "meninggal":"<?= $row["meninggal"]; ?>",
-        "last_update":"<?= $row["last_update"]; ?>",
-        "source":"<?= $row["source"]; ?>"
-      },
-      "geometry":{
-        "type":"Polygon",
-        "coordinates":[
-          [
-            <?= $row["polygon"]; ?>
-          ]
-        ]
-      }
-    }
-    <?php } ?>
 
     <?php
       $sql = "SELECT *
