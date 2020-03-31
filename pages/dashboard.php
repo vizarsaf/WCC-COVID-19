ec4c7fe398fe4b0aa2d12816ad25b04a689808ce
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-31 12:23:08 +0700

    Benerin padding

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -93,9 +93,7 @@
       <div class="col-md-12">
 
         <div class="card text-gray-100 shadow py-2" style="background:#2060A7;">
-          <div class="card-body">
-            <div class="text-xs font-weight-bold text-uppercase mb-1">Data Jakarta</div>
-          </div>
+          <h3>Data Jakarta</h3>
           <canvas class="p-4" style="height:400px;" id="piechart"></canvas>
         </div>
 
