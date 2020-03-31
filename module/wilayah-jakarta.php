2734ba460d59ede1ad11d7c1568a8dbc07585cc6
Dita Aji Pratama <ditaajipratama@protonmail.com>
2020-03-31 17:50:41 +0700

    Test js di comment

--- module/wilayah-jakarta.php
+++ module/wilayah-jakarta.php
@@ -34,16 +34,16 @@ setTimeout(function initMap() {
 
   peta.data.loadGeoJson('module/geojson.php');
   peta.data.setStyle(function(feature) {
-    if (feature.getProperty('table')=="provinsi") {
-      var provinsi = feature.getProperty('provinsi');
-      var positif = feature.getProperty('positif');
-      var sembuh = feature.getProperty('sembuh');
-      var meninggal = feature.getProperty('meninggal');
-      var last_update = feature.getProperty('last_update');
-      var color = "white";
-      if (positif == 0 && last_update != "0000-00-00 00:00:00") color = "green";
-      if (positif > 0) color = "red";
-    }
+    // if (feature.getProperty('table')=="provinsi") {
+    //   var provinsi = feature.getProperty('provinsi');
+    //   var positif = feature.getProperty('positif');
+    //   var sembuh = feature.getProperty('sembuh');
+    //   var meninggal = feature.getProperty('meninggal');
+    //   var last_update = feature.getProperty('last_update');
+    //   var color = "white";
+    //   if (positif == 0 && last_update != "0000-00-00 00:00:00") color = "green";
+    //   if (positif > 0) color = "red";
+    // }
     if (feature.getProperty('table') == "kelurahan") {
       var provinsi = feature.getProperty('provinsi');
       var kelurahan = feature.getProperty('kelurahan');
@@ -65,33 +65,33 @@ setTimeout(function initMap() {
     }
   });
   peta.data.addListener('click', function(event) {
-    if (event.feature.getProperty('table')=="provinsi") {
-      var info = "" +
-
-        "<p class='text-dark'>" +
-        "Provinsi: " + event.feature.getProperty('provinsi') +
-        "</p>" +
-
-        "<p class='text-danger'>" +
-        "Positif: " + event.feature.getProperty('positif') +
-        "</p>" +
-
-        "<p class='text-success'>" +
-        "Sembuh: " + event.feature.getProperty('sembuh') +
-        "</p>" +
-
-        "<p class='text-dark'><b>" +
-        "Meninggal: " + event.feature.getProperty('meninggal') +
-        "</b></p>" +
-
-        "<p class='text-dark'>" +
-        "Last Update: " + event.feature.getProperty('last_update') +
-        "</p>" +
-
-        "<a target='_blank' href='" + event.feature.getProperty('source') + "'>Source</a>" +
-
-      "";
-    }
+    // if (event.feature.getProperty('table')=="provinsi") {
+    //   var info = "" +
+    //
+    //     "<p class='text-dark'>" +
+    //     "Provinsi: " + event.feature.getProperty('provinsi') +
+    //     "</p>" +
+    //
+    //     "<p class='text-danger'>" +
+    //     "Positif: " + event.feature.getProperty('positif') +
+    //     "</p>" +
+    //
+    //     "<p class='text-success'>" +
+    //     "Sembuh: " + event.feature.getProperty('sembuh') +
+    //     "</p>" +
+    //
+    //     "<p class='text-dark'><b>" +
+    //     "Meninggal: " + event.feature.getProperty('meninggal') +
+    //     "</b></p>" +
+    //
+    //     "<p class='text-dark'>" +
+    //     "Last Update: " + event.feature.getProperty('last_update') +
+    //     "</p>" +
+    //
+    //     "<a target='_blank' href='" + event.feature.getProperty('source') + "'>Source</a>" +
+    //
+    //   "";
+    // }
     if (event.feature.getProperty('table')=="kelurahan") {
       var info = "" +
 
